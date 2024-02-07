a4b1f143c15f0a404c6f43f0569a85ef2841dac7
Raritetnik <mkodarenko@gmail.com>
2024-02-07 10:14:43 -0500

    Integration of react

--- src/main/java/com/example/prtfserver/controller/CatalogueController.java
+++ src/main/java/com/example/prtfserver/controller/CatalogueController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class CatalogueController {
 
     @GetMapping("/")
-    public String showHomePageView() {
-        return "homePageView";
+    public String index() {
+        return "index";
     }
 }
