a8b5f79120ec0c2c5cd533aace0b2efd70db4148
Raritetnik <mkodarenko@gmail.com>
2024-02-07 09:01:12 -0500

    Integration on git

---
+++ src/main/java/com/example/prtfserver/PrtfServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.prtfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrtfServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrtfServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prtfserver/PrtfServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prtfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrtfServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
